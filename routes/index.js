78ad3fba516fb43c2d30be0d5023dbde07b6c01f
Fortan Pireva <fortanpireva99@gmail.com>
2020-05-14 23:05:37 +0200

    Added login and signup pages

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,11 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Homepage" });
 });
 
+router.get("/login", (req, res) => {
+  res.render("public/login", { title: "Kyçja në Llogari" });
+});
+
+router.get("/signup", (req, res) => {
+  res.render("public/signup", { title: "Regjistrohu" });
+});
 module.exports = router;
