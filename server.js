36ac26eb68883f4c0c27149ec67c91a974675053
Fortan Pireva <fortanpireva99@gmail.com>
2020-05-14 12:12:09 +0200

    Initial setup

---
+++ server.js
@@ -0,0 +1,41 @@
+// nodejs modules
+const path = require("path");
+
+//package requires
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+//load .env key-value pairs in PROCESS.ENV object
+require("dotenv").config();
+// constants
+const PORT = process.env.PORT || 3000;
+const REMOTE_DB_URL = `mongodb+srv://${process.env.MONGODB_HOST}:${process.env.MONGODB_PASSWORD}@cluster0-mnwwo.mongodb.net/test?retryWrites=true&w=majority
+`;
+//root path i faqes
+app.get("/", (req, res) => {
+  const User = require("./models/UserModel");
+  User.create({
+    name: "Fortan",
+    surname: "Pireva",
+  }).then((user) => {
+    console.log(user);
+  });
+  res.render("index", { title: "Homepage" });
+});
+
+// konfigurime te aplikactionit
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname + "public")));
+
+mongoose.connect(REMOTE_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started listening on port ${PORT}`);
+});
