9254d9aa9dc2c9271ea9665f730ddbca619afdc0
labinot <labinot.dragusha@studentet.uni-pr.edu>
2020-05-23 18:44:36 +0200

    Hashed password and email auth

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,22 +1,31 @@
+const bcrypt = require('bcryptjs');
 const UserModel = require("../models/UserModel");
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  UserModel.findOne({ email }).then((user) => {
-    if (user.password === password) {
-      if (user.isAdmin) {
-        res.redirect("/admin/");
+  UserModel.findOne({ email:email })
+  .then((user) => {
+    if(!user){
+      return res.redirect('/login')
+    }
+    bcrypt
+     .compare(password,user.password)
+     .then(doMatch => {
+       if(doMatch) {
+        if (user.isAdmin) {
+          res.redirect("/admin/");
+          return;
+        }
+        res.render("user/index", {
+          title: `Mirëserdhe ${user.fullName}`,
+          fullName: user.fullName,
+        });
         return;
       }
-      res.render("user/index", {
-        title: `Mirëserdhe ${user.fullName}`,
-        fullName: user.fullName,
-      });
-      return;
-    }
-    res.redirect("/signup");
-  });
+      res.redirect("/signup");
+    })
+  })
 };
 
 exports.postSignUp = async (req, res, next) => {
@@ -26,25 +35,38 @@ exports.postSignUp = async (req, res, next) => {
   const password = req.body.confirmPassword;
   const confirmPassword = req.body.confirmPassword;
 
-  if (password !== confirmPassword || email === "") {
-    res.render("public/signup", {
-      title: "Sign up",
-    });
-  }
-  const User = new UserModel({
-    name,
-    surname,
-    email,
-    password,
-  });
-  User.save().then((user) => {
-    if (user) {
-      console.log(user);
-
-      res.render("public/login", {
-        title: "Kycu",
-      });
+   if (password !== confirmPassword || email === "") {
+     res.render("public/signup", {
+       title: "Sign up",
+     }) 
     }
-  });
+  //Same emails aren't allowed 
+  UserModel.findOne({email:email})
+    .then(userDoc => {
+      if(userDoc){
+        return res.redirect('/signup');
+      }
+      return bcrypt.hash(password, 12);
+    })
+    .then(hashedPassword => {
+      const User = new UserModel({
+        name,
+        surname,
+        email,
+        password:hashedPassword
+      }); 
+      User.save().then((user) => {
+        if (user) {
+          console.log(user);
+    
+          res.render("public/login", {
+            title: "Kycu",
+          });
+        }
+      });
+
+  }).catch(err => {
+    console.log(err);
+  })  
 };
 //test for contributor
